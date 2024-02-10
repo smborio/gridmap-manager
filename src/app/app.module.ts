d2f78d6e5f986bb8e27268b8732a67e84bbaa251
smborio <smborio@idems.international>
2024-02-10 19:13:47 -0300

    set up top left component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopLeftComponent } from './components/top-components/top-left/top-left.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopLeftComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/top-components/top-left/top-left.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-left',
+  templateUrl: './top-left.component.html',
+  styleUrls: ['./top-left.component.scss']
+})
+export class TopLeftComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
